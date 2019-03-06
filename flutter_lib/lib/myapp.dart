19ae3bc6e8de2d27776ef9b69619ff428da85fce
ym_windows <18720625976@163.com>
2019-03-07 00:18:48 +0800

    study kit

---
+++ flutter_lib/lib/myapp.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_lib/ui/page/shop/shoplist.dart';
+import 'package:flutter_lib/ui/page/notfound/notfound_page.dart';
+import 'package:flutter_lib/utils/uidata.dart';
+import 'package:flutter_lib/ui/page/dashboard/dashboard.page.dart';
+import 'package:flutter_lib/ui/page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  final materialApp = MaterialApp(
+      title: UIData.appName,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          fontFamily: UIData.quickFont,
+          primarySwatch: Colors.amber),
+      debugShowCheckedModeBanner: false,
+      showPerformanceOverlay: false,
+      home: HomePage(),
+//      localizationsDelegates: [
+//        const TranslationsDelegate(),
+//        GlobalMaterialLocalizations.delegate,
+//        GlobalWidgetsLocalizations.delegate,
+//      ],
+//      supportedLocales: [
+//        const Locale("en", "US"),
+//        const Locale("hi", "IN"),
+//      ],
+       initialRoute: UIData.notFoundRoute,
+
+      //routes
+      routes: <String, WidgetBuilder>{
+        UIData.homeRoute: (BuildContext context) => MyHomePage(),
+        UIData.shoplist: (BuildContext context) => ShopList(),
+        UIData.notFoundRoute: (BuildContext context) => NotFoundPage(),
+
+      },
+      onUnknownRoute: (RouteSettings rs) => new MaterialPageRoute(
+          builder: (context) => new NotFoundPage(
+            appTitle: UIData.coming_soon,
+//            icon: FontAwesomeIcons.solidSmile,
+            title: UIData.coming_soon,
+            message: "Under Development",
+            iconColor: Colors.green,
+          )));
+
+  @override
+  Widget build(BuildContext context) {
+    return materialApp;
+  }
+}
