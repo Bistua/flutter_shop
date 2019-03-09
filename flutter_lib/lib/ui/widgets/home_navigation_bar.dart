68084aa48d2b67a9c96da9eacd61c707bb0dfb46
ym_windows <18720625976@163.com>
2019-03-09 17:47:32 +0800

    add home navigation bar

--- flutter_lib/lib/ui/widgets/home_navigation_bar.dart
+++ flutter_lib/lib/ui/widgets/home_navigation_bar.dart
@@ -1,34 +1,11 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//主页为有状态的
-class HomePage extends StatefulWidget {
-
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return HomePageState();
-  }
-}
-
-//此处是交互
-class HomePageState extends State<HomePage> {
-  get controller => null;
-
+class HomeNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: [        BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-            ),
-            title: Text(
-              'HOME',
-            )),
+    return BottomNavigationBar(
+      items: [
         BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
@@ -43,10 +20,15 @@ class HomePageState extends State<HomePage> {
             title: Text(
               'HOME',
             )),
-        ],
-        currentIndex: 0,
-      ),appBar: AppBar(
-      title: Text('hello'),
-    ),);
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            title: Text(
+              'HOME',
+            )),
+      ],
+      currentIndex: 0,
+    );
   }
 }
