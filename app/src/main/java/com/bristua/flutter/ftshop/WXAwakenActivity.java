66124635230bded7ab7629e3d598d83bc5b29332
RushKing <1054917071@qq.com>
2019-04-18 15:10:53 +0800

    添加微信支付

--- app/src/main/java/com/bristua/flutter/ftshop/WXAwakenActivity.java
+++ app/src/main/java/com/bristua/flutter/ftshop/WXAwakenActivity.java
@@ -5,7 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.bristua.flutter.ftshop.wxapi.WXEntryActivity;
+import com.bristua.flutter.ftshop.wxapi.WXPayEntryActivity;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.modelpay.PayReq;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
@@ -13,6 +15,12 @@ public class WXAwakenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        wxLogin();
+        finish();
+    }
+
+    //微信登录
+    private void wxLogin() {
         IWXAPI api = null;
         try {
             api = WXAPIFactory.createWXAPI(WXAwakenActivity.this, WXEntryActivity.WX_APP_ID, false);
@@ -28,6 +36,43 @@ public class WXAwakenActivity extends AppCompatActivity {
         req.scope = "snsapi_userinfo";
         req.state = "wechat_zhyy";
         api.sendReq(req);
-        finish();
+    }
+
+
+    /**
+     * 微信支付
+     *
+     * @param appid     应用ID
+     * @param partnerid 商户ID
+     * @param prepayid  预支付ID
+     * @param noncestr  订单号
+     * @param timestamp 时间戳
+     * @param sign
+     */
+    private void wxPay(String appid, String partnerid, String prepayid, String noncestr,
+                       String timestamp, String sign) {
+        IWXAPI api = null;
+        try {
+            api = WXAPIFactory.createWXAPI(WXAwakenActivity.this, WXEntryActivity.WX_APP_ID, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 是否安装微信客户端
+        if (api == null || !api.isWXAppInstalled()) {
+            //ToastUtils.showToast(mView, "检查到手机未安装微信客户端");
+            return;
+        }
+        PayReq req = new PayReq();
+        req.appId = appid;
+        req.partnerId = partnerid;
+        req.prepayId = prepayid;
+        req.nonceStr = noncestr;
+        req.timeStamp = timestamp;
+        req.packageValue = "Sign=WXPay";
+        req.sign = sign;
+        req.extData = "app data"; // optional
+        // Toast.makeText(mActivity, "正常调起支付", Toast.LENGTH_SHORT).show();
+        // 在支付之前，如果应用没有注册到微信，应该先调用IWXMsg.registerApp将应用注册到微信
+        api.sendReq(req);
     }
 }
---
+++ app/src/main/java/com/bristua/flutter/ftshop/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,61 @@
+package com.bristua.flutter.ftshop.wxapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+/**
+ * 微信支付回掉
+ */
+public class WXPayEntryActivity extends Activity implements IWXAPIEventHandler {
+    private static final String TAG = "WXPayEntryActivity";
+
+    private IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        api = WXAPIFactory.createWXAPI(this, WXEntryActivity.WX_APP_ID);
+        api.handleIntent(getIntent(), this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {
+    }
+
+    @Override
+    public void onResp(BaseResp resp) {
+        if (resp.getType() == ConstantsAPI.COMMAND_PAY_BY_WX) {
+            Log.d(TAG, "onPayFinish,errCode=" + resp.errCode);
+//            AlertDialog.Builderbuilder = newAlertDialog.Builder(this);
+//            builder.setTitle(R.string.app_tip);
+        }
+        switch (resp.errCode) {
+            case BaseResp.ErrCode.ERR_OK:
+                Log.e(TAG, "微信支付 ------ 支付成功");
+                break;
+            case BaseResp.ErrCode.ERR_COMM:// 支付失败
+                Log.e(TAG, "微信支付 ------ 支付失败");
+                break;
+            case BaseResp.ErrCode.ERR_USER_CANCEL:// 支付取消
+                Log.e(TAG, "微信支付 ------ 支付取消");
+                break;
+        }
+        finish();
+    }
+}
