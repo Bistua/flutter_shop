f97b759ecda1dd05e90571463c257db69f68231c
18140265158 <5201314wc>
2019-04-28 00:48:02 +0800

    加入根据属性组合获取具体的sku信息

--- flutter_lib/lib/bridge/sku_bridge.dart
+++ flutter_lib/lib/bridge/sku_bridge.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter_lib/bridge/common_bridge.dart';
 import 'package:flutter_lib/model/Result.dart';
 
@@ -15,6 +17,23 @@ class SkuBridge {
         "arguments": {
           "method": "find_goods_sku",
           "data": {"productId": productId},
+        }
+      }
+    });
+  }
+
+/*
+  根据属性组合获取具体的sku信息
+ */
+  static Future<Result> findGoodsSkuInfo(
+      String productId, Int64List optionsIds) {
+    return Bridge.dispenser({
+      "method": "find_goods_sku_info",
+      "params": {
+        "action": component,
+        "arguments": {
+          "method": "find_goods_sku_info",
+          "data": {"productId": productId, "optionsIds": optionsIds},
         }
       }
     });
