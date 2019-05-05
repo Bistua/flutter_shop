9d8d4a5aa8849047b1e56b11e40143eacc1f9ce4
ym_windows <18720625976@163.com>
2019-05-05 21:34:31 +0800

    add http

--- flutter_lib/lib/utils/http.dart
+++ flutter_lib/lib/utils/http.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_lib/model/Banner.dart';
+import 'package:flutter_lib/model/Result.dart';
 
 class Http {
-  static Future<Images> getBanner() async {
-    var url = "https://mall.bchun.com/mall/get/app/index/image";
+
+   static const  baseUrl = "https://mall.bchun.com";
+
+   static const  banner = "/mall/get/app/index/image";
+
+  static Future<Result> getResult(String url) async {
     bool trustSelfSigned = true;
     HttpClient httpClient = new HttpClient()
       ..badCertificateCallback =
@@ -13,6 +18,33 @@ class Http {
     String result;
     try {
       var request = await httpClient.postUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.ok) {
+        var jsons = await response.transform(utf8.decoder).join();
+        Result jsonResponse = Result.fromJson(jsons);
+        return jsonResponse;
+      } else {
+        result =
+            'Error getting IP address:\nHttp status ${response.statusCode}';
+        return Result.fromJson(
+            json.encode({"code": response.statusCode, "msg": result}));
+      }
+    } catch (exception) {
+      result = exception.toString();
+      return Result.fromJson(json.encode({"code": -1, "msg": result}));
+    }
+  }
+
+
+  static Future<Images> getBanner() async {
+    bool trustSelfSigned = true;
+    HttpClient httpClient = new HttpClient()
+      ..badCertificateCallback =
+          ((X509Certificate cert, String host, int port) => trustSelfSigned);
+
+    String result;
+    try {
+      var request = await httpClient.postUrl(Uri.parse(baseUrl+banner));
       var response = await request.close();
       if (response.statusCode == HttpStatus.ok) {
         var jsons = await response.transform(utf8.decoder).join();
